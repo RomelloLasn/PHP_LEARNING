1babc0014fa8f8d6d2234c1bb65363b968c69ded
RomelloLasn <112690676+RomelloLasn@users.noreply.github.com>
2023-10-20 13:10:01 +0300

    Update books.php

--- books.php
+++ books.php
@@ -1,12 +1,42 @@
+
 <?php
-require_once("connect.php");
+
 require_once("config.php");
+require_once("connect.php");
 
- $id = $_GET["id"];
+
+$id = $_GET["id"];
 
 
-$stmt = $pdo->prepare("SELECT * FROM books WHERE id = :id");
+$stmt = $pdo->prepare('SELECT * FROM books WHERE id = :id');
 $stmt->execute(['id' => $id]);
-$book = $start->fetch();
+$book = $stmt->fetch();
+
+$stmt = $pdo->prepare('SELECT * FROM book_authors ba LEFT JOIN authors a ON ba.author_id=a.id WHERE book_id = :id');
+$stmt->execute(['id' => $id]);
+
+
 
 var_dump($book);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?= $book["title"]; ?></h1>
+    <span style="font-size: 24px;">Aasta</span>
+    <span stle="font-size: 32px"><?= $book["release_date"]; ?></span>
+    <span style="font-size: 24px;">autor</span>
+  
+
+
+    <li>
+        <?= $row['first_name']; ?> <?= $row['last_name']; ?>
+    </li>
+</body>
+</html>
