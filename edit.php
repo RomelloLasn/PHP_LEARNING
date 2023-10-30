9f9dd576e3e8eb3291fc45ef8470866d55f33ceb
RomelloLasn <112690676+RomelloLasn@users.noreply.github.com>
2023-10-30 11:41:38 +0200

    Update edit.php

--- edit.php
+++ edit.php
@@ -5,43 +5,43 @@ require_once("connect.php");
 
 
 $id = $_GET["id"];
-if ( isset($_POST['submit']) && $_POST['submit'] == 'save') {
-    $stmt = $pdo->prepare('UPDATE books SET title = :title WHERE id = :id');
-    $stmt->execute(['title' => $_POST['title'], 'id' => $id]);
 
-    header("Location: book.php?id=($id)");
-}
 
 $stmt = $pdo->prepare('SELECT * FROM books WHERE id = :id');
 $stmt->execute(['id' => $id]);
 $book = $stmt->fetch();
+var_dump($_POST);
+if ( isset($_POST["submit"]) && $_POST["submit"] == "save" ) {
+    $stmt = $pdo->prepare('UPDATE books SET title = :title, release_date = :release_date WHERE id = :id');
+    $stmt->execute(['title' => $_POST["title"], "id" => $id, 'release_date' => $_POST["year"]]);
 
-$id = $_GET["id"];
+    header("Location: book.php?id={$id}");
+
+}
+
 
 ?>
+
+<h1>Muuda</h1> <?= $id;?> <?= $book["title"]; ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>muuda</title>
+    <title>Document</title>
 </head>
 <body>
-    
-<h1>Muuda</h1> <?= $id;?> <?= $book["title"]; ?>
-    <form action="edit.php?id=<?= $id ;?>  " method="post">
-    pealkiri: <input type="text" name="title" value="<?= $book['title'] ;?>">
-    <br><br>
-    <button type="submit" name="submit" value="save">Salvesta</button>
+
+<form action="./edit.php?id=<?= $id; ?>" method="post">
+   Pealkiri:  <input type="text" name="title" value="<?= $book["title"];?>">
+   <br><br>
+   Aasta: <input type="text" name="year"  value="<?= $book["release_date"];?>">
+   <br><br>
+<button type="submit" name="submit"  value="save">Salvesta</button>
 </form>
-   
-    
-   
-    <br>
-    
-    
-    <br>
-    
+
+
+
 </body>
 </html>
-
